208abc7cfbf4260e9c3578916e4562125268c6a4
developer <developer0800@gmail.com>
2026-03-21 03:00:46 +0530

    Fix: Final forced livewire config to cloudinary

--- config/livewire.php
+++ config/livewire.php
@@ -63,11 +63,19 @@ return [
     |
     */
 
-  'temporary_file_upload' => [
-    'disk' => 'cloudinary',        // અહીં 'null' અથવા 'local' હશે, તેને બદલીને 'cloudinary' કરો
-    'preview_mimes' => ['png', 'gif', 'jpg', 'jpeg'],
-    'max_size' => 12288,
-],
+    'temporary_file_upload' => [
+        'disk' => null,        // Example: 'local', 's3'              | Default: 'default'
+        'rules' => null,       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
+        'directory' => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
+        'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
+        'preview_mimes' => [   // Supported file types for temporary pre-signed file URLs...
+            'png', 'gif', 'bmp', 'svg', 'wav', 'mp4',
+            'mov', 'avi', 'wmv', 'mp3', 'm4a',
+            'jpg', 'jpeg', 'mpga', 'webp', 'wma',
+        ],
+        'max_upload_time' => 5, // Max duration (in minutes) before an upload is invalidated...
+        'cleanup' => true, // Should cleanup temporary uploads older than 24 hrs...
+    ],
 
     /*
     |---------------------------------------------------------------------------
