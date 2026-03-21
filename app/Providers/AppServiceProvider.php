4b4ecea50207b40920f3d984e7cdb76ea3052127
developer <developer0800@gmail.com>
2026-03-21 17:49:19 +0530

    Hardcoded Cloudinary config for Render

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,17 +23,15 @@ class AppServiceProvider extends ServiceProvider
     // HTTPS ફોર્સ કરવા માટે
     \Illuminate\Support\Facades\URL::forceScheme('https');
 
-    // Force Cloudinary settings at runtime for Render deployment
+    // Complete Cloudinary bypass - override all environment issues
     config([
+        'livewire.temporary_file_upload.disk' => 'cloudinary',
         'filesystems.default' => 'cloudinary',
         'filesystems.disks.cloudinary.driver' => 'cloudinary',
-        'filesystems.disks.cloudinary.cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-        'filesystems.disks.cloudinary.api_key' => env('CLOUDINARY_API_KEY'),
-        'filesystems.disks.cloudinary.api_secret' => env('CLOUDINARY_API_SECRET'),
-        'filesystems.disks.cloudinary.url' => env('CLOUDINARY_URL'),
-        'livewire.temporary_file_upload.disk' => 'cloudinary',
-        'livewire.temporary_file_upload.rules' => 'file|max:20480',
-        'livewire.temporary_file_upload.max_upload_time' => 10,
+        'filesystems.disks.cloudinary.cloud_name' => env('CLOUDINARY_CLOUD_NAME', 'default_cloud'),
+        'filesystems.disks.cloudinary.api_key' => env('CLOUDINARY_API_KEY', 'default_key'),
+        'filesystems.disks.cloudinary.api_secret' => env('CLOUDINARY_API_SECRET', 'default_secret'),
+        'filesystems.disks.cloudinary.url' => env('CLOUDINARY_URL', 'cloudinary://default_key:default_secret@default_cloud'),
     ]);
 }
 }
