6a1dd0d4c6d8dccff77cd6c2cf6010805b899790
developer <developer0800@gmail.com>
2026-03-21 17:21:57 +0530

    Final force configuration fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,9 +19,15 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-    {
-        config(['livewire.temporary_file_upload.disk' => 'cloudinary']);
-        config(['filesystems.default' => 'cloudinary']);
-        \Illuminate\Support\Facades\URL::forceScheme('https');
-    }
+{
+    // HTTPS ફોર્સ કરવા માટે
+    \Illuminate\Support\Facades\URL::forceScheme('https');
+
+    // ગમે તે થાય, સ્ટોરેજ ક્લાઉડિનરી જ રહેવું જોઈએ
+    config([
+        'filesystems.default' => 'cloudinary',
+        'filesystems.disks.cloudinary.driver' => 'cloudinary',
+        'livewire.temporary_file_upload.disk' => 'cloudinary',
+    ]);
+}
 }
